dd7e5ab4fa4d1fcafa11f4a9630b3fe19f327e82
Astha <70913929+asthacoder@users.noreply.github.com>
2021-08-22 16:43:59 +0530

    Add files via upload

---
+++ c104mode.py
@@ -0,0 +1,45 @@
+# Python program to print
+# mode of elements
+from collections import Counter
+import csv
+
+with open('height-weight.csv', newline='') as f:
+    reader = csv.reader(f)
+    file_data = list(reader)
+
+file_data.pop(0)
+
+new_data=[]
+for i in range(len(file_data)):
+	n_num = file_data[i][1]
+	new_data.append(n_num)
+
+
+
+#Calculating Mode
+data = Counter(new_data)
+mode_data_for_range = {
+                        "50-60": 0,
+                        "60-70": 0,
+                        "70-80": 0
+                    }
+for height, occurence in data.items():
+    if 50 < float(height) < 60:
+        mode_data_for_range["50-60"] += occurence
+    elif 60 < float(height) < 70:
+        mode_data_for_range["60-70"] += occurence
+    elif 70 < float(height) < 80:
+        mode_data_for_range["70-80"] += occurence
+
+
+#declare mode_range and mode_occurrence and set it’s value to 0. Using for loop using on mode_data_for_range.items().
+#If occurrence is greater than mode_occurrence then using split() on range we get the first and second number mode. 
+# Then find the mean ofmodes and print it
+# mothe two de_range, mode_occurence = 0, 0
+
+
+for range, occurence in mode_data_for_range.items():
+    if occurence > mode_occurence:
+        mode_range, mode_occurence = [int(range.split("-")[0]), int(range.split("-")[1])], occurence
+mode = float((mode_range[0] + mode_range[1]) / 2)
+print(f"Mode is -> {mode:2f}")
